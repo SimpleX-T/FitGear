e1ef8a4504fbad1438d4626c3adffb09b8627c3e
Xcodes <ntmark2004@gmail.com>
2024-07-09 06:31:03 +0100

    Made changes to the hero bg and the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
 			},
 			backgroundImage: {
 				"hero-bg":
-					"radial-gradient(circle at center, rgba(10,10,10,0.2),rgba(10,10,10,0.6)),url('public/heroBg.jpg')",
+					"radial-gradient(circle at center, rgba(10,10,10,0.2),rgba(10,10,10,0.6)),url('https://images.unsplash.com/photo-1583454110551-21f2fa2afe61?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
 			},
 		},
 		plugins: [],
